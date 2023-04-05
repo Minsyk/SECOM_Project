b327a716427fec48e865970e683d08ea7b725a3a
Aylin Gülüm <34208555+Aylin1@users.noreply.github.com>
2023-04-05 23:06:37 +0200

    A few lines added

--- intial_analysis.R
+++ intial_analysis.R
@@ -1,17 +1,29 @@
 
-# install.packages("RCurl")
-
+install.packages("RCurl")
+install.packages("purrr")
+library(tidyverse)
 # load the library
 library(RCurl)
+
 # specify the URL for the Secom data
-urlfile <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom.data'
+sensor_urlfile <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom.data'
 
 # read data
-dataset <- read.csv("secom.data", sep=" ", header=FALSE)
+sensor_data <- read.csv(urlfile, sep=" ", header=FALSE)
 
-head(dataset)
+# shape of data
+dim(secom_data)
 
-colnames(dataset)
+# specify the URL for the Secom data
+labels_urlfile <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom_labels.data'
 
-nrow(dataset)
-ncol(dataset)
+# read data
+labels_data <- read.csv(labels_urlfile, sep=" ", header=FALSE)
+
+# shape of data
+dim(labels_data)
+
+# concatenate Sensor
+secom_data <- cbind(secom_data,df2=labels_data)
+
+head(secom_data)
